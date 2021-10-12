9ef8b73ff58f66551697842978d8699680842be9
zxk <z928385924@163.com>
2021-10-12 17:43:42 +0800

    ajv1

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -1,16 +1,29 @@
 const Ajv = require('ajv')
+const localize = require('ajv-i18n')
 const ajv = new Ajv()
+
+ajv.addKeyword('test', {
+  macro: function (schema) {
+    return {
+      minLength: 10,
+    }
+  },
+})
 
 const schema = {
   type: 'object',
   properties: {
     foo: { type: 'integer' },
     bar: { type: 'string' },
+    name: { type: 'string', test: '14341' },
   },
   required: ['foo'],
   additionalProperties: false,
 }
 
-const data = { foo: '1', bar: 'abc' }
+const data = { foo: 1, bar: 'abc', name: 'hah1232a' }
 const valid = ajv.validate(schema, data)
-if (!valid) console.log(ajv.errors)
+if (!valid) {
+  localize.zh(ajv.errors)
+  console.log(ajv.errors)
+}
