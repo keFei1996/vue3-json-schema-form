9867b904b3417d598fbaa3f91f09b2e9fa89f5fe
zxk <z928385924@163.com>
2021-10-26 11:10:30 +0800

    jest Array

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'no-prototype-builtins': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
   overrides: [
     {
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,24 +1,29 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import { defineComponent, h } from 'vue'
 
-const HelloWorld = defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-  setup(props) {
-    return () => {
-      return h('div', props.msg)
-    }
-  },
-})
+import JsonSchemaForm, { NumberField } from '../../lib'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe('JsonSchemaForm', () => {
+  it('should render correct number filed', async () => {
+    let value = ''
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'number',
+        },
+        value: value,
+        onChange: (v: any) => {
+          value = v
+        },
+      },
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    const numberFiled = wrapper.findComponent(NumberField)
+    expect(numberFiled.exists()).toBeTruthy()
+    // await numberFiled.props('onChange')('123')
+    const input = numberFiled.find('input')
+    input.element.value = '123'
+    input.trigger('input')
+    expect(value).toBe(123)
   })
 })
